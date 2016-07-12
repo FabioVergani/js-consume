c1ab5d2c375fd0152cb61d049b2a4657fd37f8f0
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-07-12 18:57:27 +0200

    Update consume.js

--- consume.js
+++ consume.js
@@ -1,7 +1,19 @@
-Array.prototype.consume=function(x){
-	var f=x,m;if(f){m=this;while(e=m.shift()){f(e,m);};};
-};
+Array.prototype.consume=function(x){var f=x,m;if(f){m=this;while(e=m.shift()){f(e,m);};};};
 
+/*
 var m=[1,2,3,4,5];
 m.consume(function(value,thearray){console.log(value,thearray);});
+
+1
+[2, 3, 4, 5]
+2
+[3, 4, 5]
+3
+[4, 5]
+4
+[5]
+5
+[]
+
+*/
 
